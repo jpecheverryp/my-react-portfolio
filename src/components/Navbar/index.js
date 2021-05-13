094e9afee5a2c408e8821017a051b10d5dc435da
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-05-13 14:07:23 -0400

    created navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
